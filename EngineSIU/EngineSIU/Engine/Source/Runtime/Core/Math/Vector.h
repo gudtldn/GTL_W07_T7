fefad660fce3ff54e58f1ce9f8ff93ba918d1008
gudtldn <gudtldn1234@gmail.com>
2025-04-25 15:59:20 +0900

    feat: FVector 클래스에 SizeSquared 및 IsNormalized 메서드 추가

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Core/Math/Vector.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Core/Math/Vector.h
@@ -169,7 +169,8 @@ public:
     bool AllComponentsEqual(float Tolerance = KINDA_SMALL_NUMBER) const;
 
     float Length() const;
-    float LengthSquared() const;
+    float SquaredLength() const;
+    float SizeSquared() const;
 
     bool Normalize(float Tolerance = SMALL_NUMBER);
 
@@ -182,7 +183,8 @@ public:
     bool IsNearlyZero(float Tolerance = SMALL_NUMBER) const;
     bool IsZero() const;
 
-    
+    bool IsNormalized() const;
+
     FString ToString() const;
     bool InitFromString(const FString& InSourceString);
 };
@@ -331,7 +333,7 @@ inline float FVector::Length() const
     return FMath::Sqrt(X * X + Y * Y + Z * Z);
 }
 
-inline float FVector::LengthSquared() const
+inline float FVector::SquaredLength() const
 {
     return X * X + Y * Y + Z * Z;
 }
