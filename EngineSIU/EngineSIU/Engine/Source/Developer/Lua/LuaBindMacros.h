7cb22c4a6d6e6efa7cacdf0afd9e3568411fd8cb
gudtldn <gudtldn1234@gmail.com>
2025-04-25 15:23:09 +0900

    feat: LuaBindMacros.h에서 멤버 변수 이름 추출 함수 추가

--- EngineSIU/EngineSIU/Engine/Source/Developer/Lua/LuaBindMacros.h
+++ EngineSIU/EngineSIU/Engine/Source/Developer/Lua/LuaBindMacros.h
@@ -1,7 +1,31 @@
 ﻿#pragma once
+#include <string_view>
 
-class LuaBindMacros
+
+namespace LuaBindUtils
 {
-public:
-    
-};
+/**
+ * 멤버 변수 포인터 형식의 문자열에서 멤버 변수 이름만 가져옵니다.
+ * @note &Test::Var -> Var
+ */
+consteval std::string_view GetMemberVarName(std::string_view RawName)
+{
+    // 멤버변수가 아닌 경우
+    if (!RawName.starts_with("&")) throw;
+
+    // 1. 마지막 '::' 찾기
+    const size_t LastColonPos = RawName.find_last_of(':');
+    if (LastColonPos == std::string_view::npos) throw;
+
+    // 2. '::' 뒤부터 끝까지 잘라내기
+    const std::string_view MemberName = RawName.substr(LastColonPos + 1);
+
+    // 3. (선택적) 이름 유효성 검사 (예: 공백 없음 등)
+    if (MemberName.empty()) throw;
+
+    return MemberName;
+}
+
+}
+
+#define LUA_BIND_MEMBER_VAR(MemberName) LuaBindUtils::GetMemberName(#MemberName), MemberName
