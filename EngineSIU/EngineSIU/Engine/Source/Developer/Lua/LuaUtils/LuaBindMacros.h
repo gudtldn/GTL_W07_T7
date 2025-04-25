71077cef2ec8af19def27e445ba8f5c75101a487
gudtldn <gudtldn1234@gmail.com>
2025-04-25 16:01:38 +0900

    chore: LuaBindMacros.h에 새로운 UserType 추가에 대한 주석 수정

--- EngineSIU/EngineSIU/Engine/Source/Developer/Lua/LuaUtils/LuaBindMacros.h
+++ EngineSIU/EngineSIU/Engine/Source/Developer/Lua/LuaUtils/LuaBindMacros.h
@@ -2,9 +2,9 @@
 #include "LuaBindUtils.h"
 
 /**
- * 
+ * Lua에 새로운 UserType을 추가합니다.
  * @param TypeName lua에서 사용될 커스텀 타입
- * @note sol::state Lua; Lua.Lua_New_UserType(FTestStruct, ...);
+ * @note Ex) sol::state Lua; Lua.Lua_New_UserType(FTestStruct, ...);
  */
 #define Lua_New_UserType(TypeName, ...) new_usertype<TypeName>(#TypeName, __VA_ARGS__)
 
