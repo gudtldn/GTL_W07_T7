09cf7728e9698556ef61fc8fa89204b11a2dbc09
gudtldn <gudtldn1234@gmail.com>
2025-04-25 15:37:59 +0900

    feat: LuaBindMacros.h에서 Lua_New_UserType 매크로 추가 및 LuaBindUtils.h의 주석 수정

--- EngineSIU/EngineSIU/Engine/Source/Developer/Lua/LuaUtils/LuaBindMacros.h
+++ EngineSIU/EngineSIU/Engine/Source/Developer/Lua/LuaUtils/LuaBindMacros.h
@@ -1,4 +1,11 @@
 ﻿#pragma once
 #include "LuaBindUtils.h"
 
-#define LUA_BIND_MEMBER_VAR(MemberName) LuaBindUtils::GetMemberName(#MemberName), MemberName
+/**
+ * 
+ * @param TypeName lua에서 사용될 커스텀 타입
+ * @note sol::state Lua; Lua.Lua_New_UserType(FTestStruct, ...);
+ */
+#define Lua_New_UserType(TypeName, ...) new_usertype<TypeName>(#TypeName, __VA_ARGS__)
+
+#define LUA_BIND_MEMBER_VAR(MemberName) LuaBindUtils::GetMemberVarName(#MemberName), MemberName
