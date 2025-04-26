e39b027de129afc01bce63f2796e3ad88509c81c
gudtldn <gudtldn1234@gmail.com>
2025-04-26 13:42:02 +0900

    feat: ALuaActor 클래스에 Lua 스크립트 경로 설정 및 가져오기 기능 추가

--- EngineSIU/EngineSIU/Engine/Source/Developer/Lua/LuaActor.h
+++ EngineSIU/EngineSIU/Engine/Source/Developer/Lua/LuaActor.h
@@ -1,16 +1,27 @@
 ﻿#pragma once
-#include "GameFramework/Actor.h"
+#include <optional>
+#include <filesystem>
+
+#include "Engine/StaticMeshActor.h"
 
 
-class ALuaActor : public AActor
+class ALuaActor : public AStaticMeshActor
 {
-    DECLARE_CLASS(ALuaActor, AActor)
+    DECLARE_CLASS(ALuaActor, AStaticMeshActor)
 
 public:
-    ALuaActor() = default;
+    ALuaActor();
 
     virtual void BeginPlay() override;
     virtual void Tick(float DeltaTime) override;
     virtual void Destroyed() override;
     virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+public:
+    std::optional<std::filesystem::path> GetScriptPath() const;
+    void SetScriptPath(const std::optional<std::filesystem::path>& Path);
+
+private:
+    // TODO: .scene에 저장을 어떻게 하지?
+    std::optional<std::filesystem::path> LuaScriptPath;
 };
