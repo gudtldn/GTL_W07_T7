0213e3e0bbce1d2da2a7f4f1fccf899f967202c2
Xerlocked <xhouse.xman@gmail.com>
2025-04-30 15:57:26 +0900

    UPDATE: Camera Shake init

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Camera/CameraModifier_CameraShake.cpp
@@ -0,0 +1,60 @@
+﻿#include "CameraModifier_CameraShake.h"
+
+#include "CameraShake.h"
+
+float UCameraModifier_CameraShake::InitializeOffset(const struct FOscillator& Param)
+{
+    switch ( Param.InitialOffset )
+    {
+        case EOO_OffsetRandom : return rand() * 2.f * PI;
+        case EOO_OffsetZero : return 0.f;
+        case EOO_MAX: return 0.f;
+        default : return 0.f;
+    }
+}
+
+void UCameraModifier_CameraShake::ReinitShake(int32 ActiveShakeIdx, float Scale)
+{
+    ActiveShakes[ActiveShakeIdx].Scale = Scale;
+    
+    UCameraShake* SourceShake = ActiveShakes[ActiveShakeIdx].SourceShake;
+
+    if (SourceShake->OscillationDuration != 0.f)
+    {
+        ActiveShakes[ActiveShakeIdx].OscillatorTimeRemaining = SourceShake->OscillationDuration;
+
+        if (ActiveShakes[ActiveShakeIdx].bBlendingOut)
+        {
+            ActiveShakes[ActiveShakeIdx].bBlendingOut = false;
+            ActiveShakes[ActiveShakeIdx].CurrentBlendOutTime = 0.f;
+
+            // stop any blendout and reverse it to a blendin
+            ActiveShakes[ActiveShakeIdx].bBlendingIn = true;
+            ActiveShakes[ActiveShakeIdx].CurrentBlendInTime = ActiveShakes[ActiveShakeIdx].SourceShake->OscillationBlendInTime * (1.f - ActiveShakes[ActiveShakeIdx].CurrentBlendOutTime / ActiveShakes[ActiveShakeIdx].SourceShake->OscillationBlendOutTime);
+        }
+    }
+}
+
+FCameraShakeInstance UCameraModifier_CameraShake::InitializeShake(UCameraShake* Shake, float Scale)
+{
+    FCameraShakeInstance Instance;
+    Instance.SourceShakeName = Shake->GetName();
+
+    
+}
+
+void UCameraModifier_CameraShake::AddCameraShake(UCameraShake* NewShake, float Scale)
+{
+}
+
+void UCameraModifier_CameraShake::RemoveCameraShake(int32 ActiveShakeIdx)
+{
+}
+
+void UCameraModifier_CameraShake::RemoveAllCameraShakes()
+{
+}
+
+void UCameraModifier_CameraShake::UpdateCameraShake(float DeltaTime, FCameraShakeInstance& ShakeInstance)
+{
+}
---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Camera/CameraShake.h
@@ -0,0 +1,73 @@
+﻿#pragma once
+#include "UObject/Object.h"
+#include "UObject/ObjectMacros.h"
+
+enum EInitialOscillatorOffset : uint8
+{
+    /** Start with random offset (default) */
+    EOO_OffsetRandom,
+
+    /** Start with zero offset */
+    EOO_OffsetZero,
+    EOO_MAX,
+};
+
+struct FOscillator
+{
+    float Amplitude;
+
+    float Frequency;
+
+    EInitialOscillatorOffset InitialOffset;
+
+    FOscillator()
+        : Amplitude(0.0f)
+        , Frequency(0.0f)
+        , InitialOffset(EOO_OffsetRandom)
+    {
+    }
+};
+
+struct FROscillator
+{
+    FOscillator Pitch;
+    FOscillator Yaw;
+    FOscillator Roll;
+};
+
+struct FVOscillator
+{
+    FOscillator X;
+    FOscillator Y;
+    FOscillator Z;
+};
+
+
+class UCameraShake : public UObject
+{
+    DECLARE_CLASS(UCameraShake, UObject)
+
+public:
+    UCameraShake();
+
+    virtual UObject* Duplicate(UObject* InOuter) override;
+
+    bool bSingleInstance;
+
+    /** Duration in seconds of current screen shake. (v < 0) means indefinite, (v = 0) means no oscillation */
+    float OscillationDuration;
+
+    float OscillationBlendInTime;
+
+    float OscillationBlendOutTime;
+
+    /** Rotation oscillation */
+    FROscillator RotationOscillator;
+    
+    /** Positional oscillation */
+    FVOscillator LocationOscillator;
+
+    virtual float GetRotOscillationMagnitude();
+
+    virtual float GetLocOscillationMagnitude();
+};
