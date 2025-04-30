23d788c97768c2819334adb4abdf32092cd4c1c3
gudtldn <gudtldn1234@gmail.com>
2025-04-30 14:59:03 +0900

    feat: 기본적인 카메라와 관련된 컴포넌트 추가

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Camera/PlayerCameraManager.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "GameFramework/Actor.h"
+
+class APlayerController;
+
+
+class APlayerCameraManager : public AActor
+{
+    DECLARE_CLASS(APlayerCameraManager, AActor)
+
+public:
+    APlayerCameraManager() = default;
+
+public:
+    /** APlayerCameraManager를 소유하고 있는 APlayerController */
+    APlayerController* PCOwner;
+};
---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/GameFramework/Pawn.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include "Actor.h"
+
+
+class APawn : public AActor
+{
+    DECLARE_CLASS(APawn, AActor)
+
+public:
+    APawn() = default;
+};
