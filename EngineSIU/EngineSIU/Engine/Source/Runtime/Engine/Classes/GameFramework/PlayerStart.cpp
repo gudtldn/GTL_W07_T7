11978c3240428b0aa4cabaf28adcf07d7f1a85c6
gudtldn <gudtldn1234@gmail.com>
2025-04-30 23:46:57 +0900

    feat: PlayerStart 클래스에 캡슐 컴포넌트 추가 및 초기화 로직 구현

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/GameFramework/PlayerStart.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/GameFramework/PlayerStart.cpp
@@ -1,1 +1,12 @@
 ﻿#include "PlayerStart.h"
+#include "Components/Collision/CapsuleComponent.h"
+#include "InteractiveToolsFramework/BaseGizmos/GizmoArrowComponent.h"
+
+
+APlayerStart::APlayerStart()
+{
+    CapsuleComponent = AddComponent<UCapsuleComponent>(TEXT("CapsuleComponent"));
+    CapsuleComponent->SetCapsuleSize(40.0f, 92.0f);
+    CapsuleComponent->SetCollisionEnabled(false);
+    SetRootComponent(CapsuleComponent);
+}
--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/GameFramework/PlayerStart.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/GameFramework/PlayerStart.h
@@ -1,7 +1,22 @@
 ﻿#pragma once
+#include "Actor.h"
 
-class PlayerStart
+
+class UCapsuleComponent;
+class UGizmoArrowComponent;
+
+class APlayerStart : public AActor
 {
+    DECLARE_CLASS(APlayerStart, AActor)
+
 public:
-    
+    APlayerStart();
+
+public:
+    UCapsuleComponent* GetCapsuleComponent() const { return CapsuleComponent; }
+
+public:
+    UCapsuleComponent* CapsuleComponent;
+
+    FName PlayerStartTag;
 };
