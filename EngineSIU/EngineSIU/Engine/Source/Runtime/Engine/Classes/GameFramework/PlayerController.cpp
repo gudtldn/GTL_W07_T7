07ed2817c06df1e15f06635a2bb2997236424190
gudtldn <gudtldn1234@gmail.com>
2025-05-01 01:20:06 +0900

    feat: APlayerController 클래스에 PlayerCameraManager 관련 메서드 및 필드 추가

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/GameFramework/PlayerController.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/GameFramework/PlayerController.cpp
@@ -1,1 +1,18 @@
 ﻿#include "PlayerController.h"
+#include "Camera/PlayerCameraManager.h"
+#include "World/World.h"
+
+
+void APlayerController::SpawnPlayerCameraManager()
+{
+    PlayerCameraManager = GetWorld()->SpawnActor<APlayerCameraManager>(PlayerCameraManagerClass);
+
+    if (PlayerCameraManager != nullptr)
+    {
+        PlayerCameraManager->InitializeFor(this);
+    }
+    else
+    {
+        UE_LOG(ELogLevel::Warning, "[LogPlayerController]Couldn't Spawn PlayerCameraManager for Player!!");
+    }
+}
