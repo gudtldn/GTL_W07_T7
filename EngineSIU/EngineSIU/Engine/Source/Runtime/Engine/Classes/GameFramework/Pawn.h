08400320ce0cb9725af3b63ba20d98c0a4406b58
gudtldn <gudtldn1234@gmail.com>
2025-04-30 19:27:19 +0900

    feat: Pawn 클래스에 GetController 메서드 및 Controller 멤버 변수 추가

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/GameFramework/Pawn.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/GameFramework/Pawn.h
@@ -1,6 +1,8 @@
 ﻿#pragma once
 #include "Actor.h"
 
+class AController;
+
 
 class APawn : public AActor
 {
@@ -8,4 +10,16 @@ class APawn : public AActor
 
 public:
     APawn() = default;
+
+public:
+    FORCEINLINE AController* GetController() const { return Controller; }
+
+    template <typename T>
+    T* GetController() const
+    {
+        return Cast<T>(GetController());
+    }
+
+protected:
+    AController* Controller;
 };
